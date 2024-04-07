f30f29ff90c2dc44153e6e56a87521475c63df35
nitish0104 <nitishdalvi1@gmail.com>
2024-04-07 21:24:40 +0530

    color change

--- src/components/contactcomponent.jsx
+++ src/components/contactcomponent.jsx
@@ -22,10 +22,10 @@ const ContactComponent = () => {
   };
 
   return (
-    <section id="contact" className="bg-white  md:py-8 sm:py-4 h-screen " >
+    <section id="contact" className="bg-[#331C52]  md:py-8 sm:py-4 h-screen " >
     <div class="container px-6 py-4 mx-auto">
         <div className='mt-8'>
-        <h1 className="text-2xl md:text-4xl font-bold text-center mb-8 relative">
+        <h1 className="text-2xl md:text-4xl font-bold text-center mb-8 relative text-white">
         <span className="absolute left-0 top-1/2 transform -translate-y-1/2 bg-black h-0.5 w-6"></span>
        Contact Me
         <span className="absolute right-0 top-1/2 transform -translate-y-1/2 bg-black h-0.5 w-6"></span>
@@ -35,7 +35,7 @@ const ContactComponent = () => {
         <div class="pb-6 grid grid-cols-1 gap-6 my-24 lg:grid-cols-2  ">
             <div class="grid grid-cols-1 gap-6 md:grid-cols-2">
                 <div>
-                    <span class="inline-block p-3 text-blue-500 rounded-full bg-blue-100/80 ">
+                    <span class="inline-block p-3 text-white rounded-full bg-blue-100/80 ">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75" />
                         </svg>
@@ -43,11 +43,11 @@ const ContactComponent = () => {
 
                     <h2 class="mt-4 text-base font-medium text-gray-800 ">Email</h2>
                     <p class="mt-2 text-sm text-gray-500 dark:text-gray-400">Our friendly team is here to help.</p>
-                    <p class="mt-2 text-sm text-blue-500 dark:text-blue-400">hello@merakiui.com</p>
+                    <p class="mt-2 text-sm text-white dark:text-blue-400">hello@merakiui.com</p>
                 </div>
 
                 <div>
-                    <span class="inline-block p-3 text-blue-500 rounded-full bg-blue-100/80 ">
+                    <span class="inline-block p-3 text-white rounded-full bg-blue-100/80 ">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z" />
                             <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z" />
@@ -56,11 +56,11 @@ const ContactComponent = () => {
                     
                     <h2 class="mt-4 text-base font-medium text-gray-800 ">Live chat</h2>
                     <p class="mt-2 text-sm text-gray-500 dark:text-gray-400">Our friendly team is here to help.</p>
-                    <p class="mt-2 text-sm text-blue-500 dark:text-blue-400">Start new chat</p>
+                    <p class="mt-2 text-sm text-white dark:text-blue-400">Start new chat</p>
                 </div>
 
                 <div>
-                    <span class="inline-block p-3 text-blue-500 rounded-full bg-blue-100/80 ">
+                    <span class="inline-block p-3 text-white rounded-full bg-blue-100/80 ">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z" />
                             <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z" />
@@ -69,11 +69,11 @@ const ContactComponent = () => {
                     
                     <h2 class="mt-4 text-base font-medium text-gray-800 ">Office</h2>
                     <p class="mt-2 text-sm text-gray-500 dark:text-gray-400">Come say hello at our office HQ.</p>
-                    <p class="mt-2 text-sm text-blue-500 dark:text-blue-400">100 Smith Street Collingwood VIC 3066 AU</p>
+                    <p class="mt-2 text-sm text-white dark:text-blue-400">100 Smith Street Collingwood VIC 3066 AU</p>
                 </div>
 
                 <div>
-                    <span class="inline-block p-3 text-blue-500 rounded-full bg-blue-100/80 ">
+                    <span class="inline-block p-3 text-white rounded-full bg-blue-100/80 ">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 002.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 01-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 00-1.091-.852H4.5A2.25 2.25 0 002.25 4.5v2.25z" />
                         </svg>
@@ -81,11 +81,11 @@ const ContactComponent = () => {
                     
                     <h2 class="mt-4 text-base font-medium text-gray-800 ">Phone</h2>
                     <p class="mt-2 text-sm text-gray-500 dark:text-gray-400">Mon-Fri from 8am to 5pm.</p>
-                    <p class="mt-2 text-sm text-blue-500 dark:text-blue-400">+1 (555) 000-0000</p>
+                    <p class="mt-2 text-sm text-white dark:text-blue-400">+1 (555) 000-0000</p>
                 </div>
             </div>
 
-            <div class=" rounded-lg bg-gray-50 ">
+            <div class=" rounded-lg bg-[#331C52] text-white ">
                <form onSubmit={handleSubmit} className="space-y-4">
             <div>
               <label htmlFor="name" className="block mb-1 font-semibold">Name</label>
@@ -123,7 +123,7 @@ const ContactComponent = () => {
                 required
               ></textarea>
             </div>
-            <button type="submit" className="bg-blue-500 w-full  text-white px-4 py-2 rounded-md hover:bg-blue-600 transition-colors">
+            <button type="submit" className="hover:bg-[#291C3A] w-full font-semibold  text-white px-4 py-2 rounded-md bg-[#8121D0] transition-colors">
               Submit
             </button>
           </form>
